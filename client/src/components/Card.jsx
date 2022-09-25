784f29aa3ab38e25bd27dccb5be1ada53fdc291b
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-09-26 00:11:17 +0200

    refact: build navbar component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import CommentIcon from '@mui/icons-material/Comment';
+import ShareIcon from '@mui/icons-material/Share';
+import InfoIcon from '@mui/icons-material/Info';
+
+const Container = styled.div``
+const Info = styled.div``
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <Container>
+        <Info>
+            card
+        </Info>
+    </Container>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Badge } from '@mui/material';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    padding: 1rem 0;
+    background-color: #21b2ab;
+`
+const Logo = styled.span`
+    color: white;
+    font-weight: 600;
+`
+const Icons = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+`
+const Icon = styled.div``
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+        <Logo>NABIL DEV</Logo>
+        <Icons>
+            <Icon>
+                <Badge badgeContent={4} color="error">
+                    <NotificationsIcon sx={{color:'white'}}/>
+                </Badge>
+            </Icon>
+            <Icon>
+                <Badge badgeContent={4} color="error">
+                    <LocalPostOfficeIcon sx={{color:'white'}}/>
+                </Badge>
+            </Icon>
+            <Icon>
+                <Badge badgeContent={4} color="error">
+                    <SettingsIcon sx={{color:'white'}}/>
+                </Badge>
+            </Icon>
+        </Icons>
+    </Container>
   )
 }
 
